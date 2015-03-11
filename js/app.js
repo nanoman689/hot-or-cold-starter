95efaad23ebcbf2b19a100774ae7d0db1d809877
Nanoman <nanoman689@gmail.com>
2015-03-10 21:06:07 -0700

    Started the game
    - Got the guess button to work
    - App now picks a random number between 1 and 100
    - Started the function to check the guess against the random number.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
 	
 	/*--- Display information modal box ---*/
@@ -12,6 +11,42 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	/*--- Get the number ---*/
+  	
+  	$('input#guessButton').click(function() {
+  		$('#userGuess').val;
+  		var userGuessNumber = +document.getElementById('userGuess').value;
+  		console.log("You guessed "+ userGuessNumber);
+  		return false;
+
+  		checkHotCold(userGuessNumber);
+
+  	});
+
+  	/*--- Compair the guess and the actual number ---*/
+
 });
+
+
+/*-- Random Number Picker --*/
+
+var hotNumber = Math.floor((Math.random() * 100) + 1);
+
+console.log(hotNumber);
+
+
+/*--- Function to check the number ---*/
+
+function checkHotCold(checkNumber){
+	if (hotNumber === userGuessNumber) {
+	console.log("You Got It!");	
+	}else if (hotNumber <= userGuessNumber) {
+	console.log("Cold");
+	}else {
+	console.log("So Cold");
+	}
+}
+
+
 
 
